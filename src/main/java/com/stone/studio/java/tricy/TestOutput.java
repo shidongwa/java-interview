520b7e9c577e50a467df70c82a1b4cb9dd8b722f
Donghua <shidonghua@meituan.com>
2020-01-05 10:04:24 +0800

    add java tricky question

---
+++ src/main/java/com/stone/studio/java/tricy/TestOutput.java
@@ -0,0 +1,9 @@
+package com.stone.studio.java.tricy;
+
+public class TestOutput {
+
+    public static void main(String[] args) {
+        //the line below this gives an output, comment executed, unicode 000d stands for RETURN
+        //\u000d System.out.println("comment executed");
+    }
+}
---
+++ src/main/java/com/stone/studio/java/tricy/TestPlus.java
@@ -0,0 +1,10 @@
+package com.stone.studio.java.tricy;
+
+public class TestPlus {
+
+    public static void main(String[] args) {
+        int i = 10 + +11 - -12 + +13 - -14 + +15;
+        // 75, 加减号不能和正负号连起来写，否则有编译错误
+        System.out.println(i);
+    }
+}
